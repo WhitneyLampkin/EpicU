bb8bf133899ffe82691a137bd11c915ae07c6fbd
WLampkin <wlampkin.davis@hotmail.com>
2016-10-15 13:49:12 -0500

    commit

---
+++ EpicULocalDBExample/EpicULocalDBExample/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EpicULocalDBExample
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LocaldbEntities1 db = new LocaldbEntities1();
+
+            var customers = db.Tables;
+
+            string result = " ";
+
+            foreach (var customer in customers)
+            {
+                result += "<p>" + customer.Name + "<p/>";
+            }
+
+            resultLabel.Text = result;
+        }
+    }
+}
---
+++ EpicULocalDBExample/EpicULocalDBExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EpicULocalDBExample.Global" Language="C#" %>
