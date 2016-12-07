81212e6d0ab7ce9dcfb350b475f4542581cf38fc
Whitney Lampkin <Whitney Lampkin@DESKTOP-PTCV97N>
2016-12-07 14:16:18 -0600

    Added HYE-Question Form

---
+++ HYE_SocialNetwork/HYE_SocialNetwork/Controllers/QuestionsController.cs
@@ -0,0 +1,21 @@
+﻿using HYE_SocialNetwork.Models;
+using System.Web.Mvc;
+
+namespace HYE_SocialNetwork.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        private ApplicationDbContext db;
+
+        public QuestionsController()
+        {
+            db = new ApplicationDbContext();       
+        }
+
+        // GET: Questions
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
