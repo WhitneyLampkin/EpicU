78da0665391dd8a564fbeb94bf9b9b7f1586e1e7
Whitney Lampkin <wlampkin.davis@hotmail.com>
2016-12-15 18:06:28 -0600

    Initial

---
+++ LimitedKicksStore/LimitedKicksStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LimitedKicksStore.Startup))]
+namespace LimitedKicksStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ MyMVC/MyMVC/Models/NewsItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMVC.Models
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
