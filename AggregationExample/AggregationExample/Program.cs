a6ab359ea6c421f52796b32062fe69bf804650e7
Whitney Lampkin <Whitney Lampkin@DESKTOP-PTCV97N>
2017-01-08 09:57:05 -0600

    Aggregation Relationship Example

--- AggregationExample/AggregationExample/Program.cs
+++ AggregationExample/AggregationExample/Program.cs
@@ -10,6 +10,76 @@ namespace AggregationExample
     {
         static void Main(string[] args)
         {
+            var car1 = new Automobile();
+            var car2 = new Automobile();
+            var car3 = new Automobile();
+
+            var person = new Person() { Name = "Whitney" };
+
+            var fleet = new Fleet();
+
+            fleet.Automobiles.Add(car1);
+            fleet.Automobiles.Add(car2);
+            fleet.Automobiles.Add(car3);
+
+            fleet.Owner = person;
+
+            foreach (var auto in fleet.Automobiles)
+            {
+                auto.Display();
+            }
+
+            fleet.Owner.Display();
+
+            //IMPORTANT
+            fleet = null; //Getting ride of fleet but will the cars or person go away?
+
+            Console.WriteLine("Do the aggregated objects still live?");
+            //The answer is yes. The fleet went away but the other objects still assisted.
+
+
+            car1.Display();
+            car2.Display();
+            car3.Display();
+
+            person.Display();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Fleet
+    {
+        public string Name { get; set; }
+
+        public List<Automobile> Automobiles { get; set; }
+        public Person Owner { get; set; }
+
+        public Fleet()
+        {
+            Automobiles = new List<Automobile>();
+        }
+    }
+
+    class Automobile
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine($"Auto: {Make} {Model} {Year}");
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine($"Person: {Name}");
         }
     }
 }
