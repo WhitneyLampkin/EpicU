9ddd396c4cfbfecd99f7a2c2342c5313ed0878b0
Whitney Lampkin <Whitney Lampkin@DESKTOP-PTCV97N>
2017-01-07 19:50:46 -0600

    Added example of Implementation Inheritance

--- Implementation Inheritance/Implementation Inheritance/Program.cs
+++ Implementation Inheritance/Implementation Inheritance/Program.cs
@@ -10,6 +10,52 @@ namespace Implementation_Inheritance
     {
         static void Main(string[] args)
         {
+            var car = new Automobile()
+            {
+                Make = "Toyota",
+                Model = "4Runer",
+                Year = 2010
+            };
+
+            car.Display();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            var suv = new SUV()
+            {
+                Make = "GMC",
+                Model = "Yukon",
+                Year = 2013,
+                DriveMechanism = "4 wheel drive"
+            };
+
+            suv.Display();
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+     }
+
+    public class Automobile
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public void Display()
+        {
+            Console.Write($"Auto: {Make} {Model} {Year}");
         }
     }
+
+    public class SUV : Automobile
+    {
+        public string DriveMechanism { get; set; }
+
+        public new void Display()
+        {
+            Console.Write($"SUV: {Make} {Model} {Year} {DriveMechanism}");
+        }
+    }
+
 }
