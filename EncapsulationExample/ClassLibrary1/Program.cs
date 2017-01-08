0925290b22f18da1691ccec8361f2f72f17d0ad6
Whitney Lampkin <Whitney Lampkin@DESKTOP-PTCV97N>
2017-01-07 19:10:36 -0600

    Added notes and example to Encapsulation project

--- EncapsulationExample/ClassLibrary1/Program.cs
+++ EncapsulationExample/ClassLibrary1/Program.cs
@@ -10,7 +10,47 @@ namespace EncapsulationExample
     {
         static void Main(string[] args)
         {
-
+            var car = new Car() { MyProperty = 7 };
+            //Notice that we cannot get to CreateDisplay.
+            //We can only access it through the public interface Display();
+            car.Display();
+            Console.ReadLine();
         }
+    }
+
+    class Car
+    {
+        private int myVar;
+
+        public int MyProperty
+        {
+            //We can encapsulate/hide myVar with the following getters/setters
+            get
+            { //Only reveal to users with certain rights in the system.
+              //if (User.Role = "Admin")
+                return myVar;
+            }
+            set
+            {
+                //Here we can check to see if this value makes sense with validation.
+                //if (value < 0 || > 2000)
+                myVar = value;
+            }
+        }
+        
+        public void Display()
+        {
+            var value = CreateDisplay();
+            Console.WriteLine(value);
+        }
+
+        //Private Helper Method used only in teh car class.
+        private string CreateDisplay()
+        {
+            return $"Car: {myVar}";
+        }
+
+        //public int MyOtherProperty { get; set; }
+
     }
 }
